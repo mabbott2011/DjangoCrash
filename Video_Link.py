ab1c83ab9d1da1f7ef2e5e2a6d5868a3a5fe8edd
Martin Abbott <Mabbott2011@gmail.com>
2018-05-29 13:44:02 -0500

    Added import of pyperclip to allow copying to clipboard

--- Video_Link.py
+++ Video_Link.py
@@ -1,5 +1,12 @@
+#pip install pyperclip
+import pyperclip
+
 link = "https://www.youtube.com/watch?v=D6esTdOLXh4"
 
+
+pyperclip.copy(link)
+print ('Link has been copied to your clipboard')
 print ('Watch the video here:',link)
 
-print('testing push/pull')
+#I don't need this test
+#print('testing push/pull')
